260c69f2d452dd487a15e52bb5477dd969b72e3b
lvlivingston <lvlivingston@gmail.com>
2023-12-04 11:48:09 -0600

    exercise 2

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 # Exercise 1
 
 students = ['Anna', 'Brian', 'Caroline']
-print(students[1])
-print(students[-1])
+print(f"The second student's name is {students[1]}.")
+print(f"The last student's name is {students[-1]}.")
 
 
 
 
 # Exercise 2
 
-
-
+foods = ('apple', 'banana', 'carrot')
+for food in foods:
+    print(f"{food.capitalize()} is a good food.")
